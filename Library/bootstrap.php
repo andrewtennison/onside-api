49e10ddc75623adb973ed7ceb649faa3dcfe5fb6
Isaac Scott <isaac.scott@gmail.com>
2011-09-24 19:15:18 +0100

    Namespaces added to files

--- Library/bootstrap.php
+++ Library/bootstrap.php
@@ -5,5 +5,5 @@ ini_set('display_errors', false);
 // Constants
 define('APPLICATION_BASE', __DIR__);
 
-
 // Autoloading
+set_include_path(APPLICATION_BASE . PATH_SEPARATOR . get_include_path());
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,3 +3,8 @@ require_once '../Library/bootstrap.php';
 
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
+
+// Constants
+
+// Autoloading
+set_include_path(__DIR__ . PATH_SEPARATOR . get_include_path());
