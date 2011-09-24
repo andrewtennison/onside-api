d2dc596b57b39da6931199f4c624895a34251790
Isaac Scott <isaac.scott@gmail.com>
2011-09-24 19:07:59 +0100

    Bootstrap updated

--- Library/bootstrap.php
+++ Library/bootstrap.php
@@ -1,1 +1,9 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
+ini_set('display_errors', false);
+
+// Constants
+define('APPLICATION_BASE', __DIR__);
+
+
+// Autoloading
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,1 +1,5 @@
 <?php
+require_once '../Library/bootstrap.php';
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
