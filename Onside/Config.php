cce716db19d0c6ebf75b770e5df4f524a5d538fd
Isaac Scott <isaac.scott@anobii.com>
2011-10-19 21:03:24 +0100

    Working tests for config

--- Onside/Config.php
+++ Onside/Config.php
@@ -10,20 +10,18 @@ abstract class Config
         $content = file_get_contents(APPLICATION_CONFIG . '/' . $class);
         $lines = explode("\n", $content);
         foreach ($lines as $line) {
+            // create entries
+            if ($store) {
+                if (!preg_match('/^;/', $line) && preg_match('/^([0-9a-zA-Z\.]+) = (.+)$/', $line, $matches)) {
+                    $this->setValue($matches[1], $matches[2]);
+                }
+            }
             // start of section
             if (preg_match('/^\[(.+) : (.+)\]$/', $line, $matches) || preg_match('/^\[(.+)\]$/', $line, $matches)) {
-echo print_r($matches, true) . "\n";
                 if ($matches[1] == $env) {
                     $store = true;
                 } else {
                     $store = false;
-                }
-            }
-            // create entries
-            if ($store) {
-                if (!preg_match('/^;/', $line) && preg_match('/^([0-9a-zA-Z\.]+) = (.+)$/', $line, $matches)) {
-echo print_r($matches, true) . "\n";
-                    $this->setValue($matches[1], $matches[2]);
                 }
             }
         }
@@ -31,7 +29,14 @@ echo print_r($matches, true) . "\n";
     
     protected function setValue($key, $value)
     {
-        $key = str_replace('.', '->', $key);
-        $this->$key = $value;
+        $ref =& $this;
+        $keys = explode('.', $key);
+        foreach ($keys as $key) {
+            $ref =& $ref->$key;
+            if (!isset($ref)) {
+                $ref = (object)array();
+            }
+        }
+        $ref = $value;
     }
 }
--- Tests/ConfigTest.php
+++ Tests/ConfigTest.php
@@ -2,12 +2,22 @@
 namespace Tests;
 use \Tests\Test;
 use Onside\Config\Common;
+use Onside\Config\Sources;
 
 class ConfigTest extends Test
 {
     public function testCommonIni()
     {
         $config = new Common('development');
-echo print_r($config, true) . "\n";
+        $this->assertInstanceOf('\Onside\Config', $config);
+        $this->assertObjectHasAttribute('db', $config);
+        $this->assertObjectHasAttribute('log', $config);
+    }
+    
+    public function testSourcesIni()
+    {
+        $sources = new Sources('development');
+        $this->assertInstanceOf('\Onside\Config', $sources);
+        $this->assertObjectHasAttribute('rss', $sources);
     }
 }
