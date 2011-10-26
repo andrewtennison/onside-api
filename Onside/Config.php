a97c19e16d5be15bd5e621240d6cc0af65178f48
Isaac Scott <isaac.scott@anobii.com>
2011-10-26 21:05:07 +0100

    Refactored config loading and tests

--- Onside/Config.php
+++ Onside/Config.php
@@ -1,34 +1,67 @@
 <?php
 namespace Onside;
 
-abstract class Config
+class Config
 {
-    public function __construct($env)
+    public function __construct($env, $filename)
     {
+        assert('null !== $env && null !== $filename');
+
         $store = false;
-        $class = str_replace('Onside\Config\\', '', get_class($this)) . '.ini';
-        $content = file_get_contents(APPLICATION_CONFIG . '/' . $class);
+        // TODO: throw exception if file doesn't exists
+        $content = file_get_contents(APPLICATION_CONFIG . '/' . $filename);
         $lines = explode("\n", $content);
-        foreach ($lines as $line) {
-            // create entries
-            if ($store) {
-                if (!preg_match('/^;/', $line) && preg_match('/^([0-9a-zA-Z\.]+) = (.+)$/', $line, $matches)) {
-                    $this->setValue($matches[1], $matches[2]);
-                }
-            }
-            // start of section
-            if (preg_match('/^\[(.+) : (.+)\]$/', $line, $matches) || preg_match('/^\[(.+)\]$/', $line, $matches)) {
-                if ($matches[1] == $env) {
-                    $store = true;
-                } else {
-                    $store = false;
+        $array = $this->arrayOfSections($lines);
+        
+        // TODO: throw exception if $env is not in $array
+        $sections = array($env);
+        while(null !== $array[$env]['parent']) {
+            $sections[] = $array[$env]['parent'];
+            $env = $array[$env]['parent'];
+        }
+        rsort($sections);
+        
+        $this->setValues($array, $sections);
+    }
+    
+    protected function setValues(array $array, array $sections)
+    {
+        foreach ($sections as $section) {
+            // TODO: throw an exception
+            if (!is_array($array[$section]['lines'])) {
+            } else {
+                foreach ($array[$section]['lines'] as $line) {
+                    $this->setValue($line[0], $line[1]);
                 }
             }
         }
     }
     
+    protected function arrayOfSections($lines)
+    {
+        $ref = null;
+        $array = array();
+        foreach ($lines as $line) {
+            // create entries
+            if (null !== $ref) {
+                if (!preg_match('/^;/', $line) && preg_match('/^([0-9a-zA-Z\.]+) = (.+)$/', $line, $matches)) {
+                    $ref[] = array($matches[1], $matches[2]);
+                }
+            }
+            // start of section
+            if (preg_match('/^\[(.+) : (.+)\]$/', $line, $matches) || preg_match('/^\[(.+)\]$/', $line, $matches)) {
+                $array[$matches[1]]['parent'] = count($matches) === 3 ? $matches[2] : null;
+                $array[$matches[1]]['lines'] = array();
+                $ref =& $array[$matches[1]]['lines'];
+            }
+        }
+        
+        return $array;
+    }
+    
     protected function setValue($key, $value)
     {
+        $value = str_replace('"', '', $value);
         $ref =& $this;
         $keys = explode('.', $key);
         foreach ($keys as $key) {
--- Tests/ConfigTest.php
+++ Tests/ConfigTest.php
@@ -1,22 +1,36 @@
 <?php
 namespace Tests;
 use \Tests\Test;
-use Onside\Config\Common;
-use Onside\Config\Sources;
+use Onside\Config;
 
 class ConfigTest extends Test
 {
-    public function testCommonIni()
+    public function provideEnvironments()
     {
-        $config = new Common('development');
+        return array(
+            array('production'),
+            array('staging'),
+            array('development'),
+        );
+    }
+    
+    /**
+     * @dataProvider provideEnvironments
+     */
+    public function testCommonIni($env)
+    {
+        $config = new Config($env, 'Common.ini');
         $this->assertInstanceOf('\Onside\Config', $config);
         $this->assertObjectHasAttribute('db', $config);
         $this->assertObjectHasAttribute('log', $config);
     }
     
-    public function testSourcesIni()
+    /**
+     * @dataProvider provideEnvironments
+     */
+    public function testSourcesIni($env)
     {
-        $sources = new Sources('development');
+        $sources = new Config($env, 'Sources.ini');
         $this->assertInstanceOf('\Onside\Config', $sources);
         $this->assertObjectHasAttribute('rss', $sources);
     }
