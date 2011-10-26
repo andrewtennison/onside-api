bd1a307509a3eda5257a84a2c52c34078dd62154
Isaac Scott <isaac.scott@anobii.com>
2011-10-26 21:06:15 +0100

    Updated bootstrap to allow access to $db globally for the moment

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,31 @@
 <?php
 use \Onside\Autoloader;
+use \Onside\Db;
 
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('display_errors', false);
 
 // Constants
 define('APPLICATION_BASE', __DIR__);
-defined('APPLICATION_ENV') or define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
+define('APPLICATION_ENV', 'development');
+//defined('APPLICATION_ENV') or define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
 define('APPLICATION_CONFIG', APPLICATION_BASE . '/Config');
+
+// TODO: General error/exception handling
+
 
 // Autoloading
 set_include_path(APPLICATION_BASE . PATH_SEPARATOR . get_include_path());
 
 require_once APPLICATION_BASE . '/Onside/Autoloader.php';
 spl_autoload_register(array(new Autoloader(), 'loader'), true);
+
+// Load required config
+$commonConfig = new \Onside\Config(APPLICATION_ENV, 'Common.ini');
+$sourcesConfig = new \Onside\Config\Common(APPLICATION_ENV, 'Sources.ini');
+
+//die(print_r($commonConfig, true));
+//echo '$commonConfig->db->default->dsn: ' . $commonConfig->db->default->dsn . "\n";
+//echo '$commonConfig->db->default->username: ' . $commonConfig->db->default->username . "\n";
+//echo '$commonConfig->db->default->passwd: ' . $commonConfig->db->default->passwd . "\n";
+$db = new Db($commonConfig->db->default->dsn, $commonConfig->db->default->username, $commonConfig->db->default->passwd);
