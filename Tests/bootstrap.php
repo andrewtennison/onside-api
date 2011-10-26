7603a090c34cd18c6384aa44aedbbd57cda87f0c
Isaac Scott <isaac.scott@anobii.com>
2011-10-26 17:24:42 +0100

    Updated bootstrap for tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -13,3 +13,45 @@ ini_set('display_errors', true);
 // Autoloading
 set_include_path(APPLICATION_BASE . '/Tests' . PATH_SEPARATOR . get_include_path());
 set_include_path(APPLICATION_BASE . '/Tests/Api' . PATH_SEPARATOR . get_include_path());
+
+// Reset database
+use \Onside\Db;
+/**
+use \Onside\Mapper\Article;
+use \Onside\Mapper\Channel;
+use \Onside\Mapper\Discussion;
+use \Onside\Mapper\Event;
+use \Onside\Mapper\User;
+use \Onside\Model\Article;
+use \Onside\Model\Channel;
+use \Onside\Model\Discussion;
+use \Onside\Model\Event;
+use \Onside\Model\User;
+ */
+/*
+$db = new Db('mysql:host=127.0.0.1;user=onside;pass=onside;dbname=onside_unittest', 'onside', 'onside');
+$db->setAttribute(Db::ATTR_ERRMODE, Db::ERRMODE_EXCEPTION);
+
+$mapper = new \Onside\Mapper\Article($db);
+$model = \Onside\Model\Article::getModelFromArray(array());
+$sql = $model->getDropSQL() . ";\n";
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\Channel($db);
+$model = \Onside\Model\Channel::getModelFromArray(array());
+$sql .= $model->getDropSQL() . ";\n";
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\Discussion($db);
+$model = \Onside\Model\Discussion::getModelFromArray(array());
+$sql .= $model->getDropSQL() . ";\n";
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\Event($db);
+$model = \Onside\Model\Event::getModelFromArray(array());
+$sql .= $model->getDropSQL() . ";\n";
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\User($db);
+$model = \Onside\Model\User::getModelFromArray(array());
+$sql .= $model->getDropSQL() . ";\n";
+$sql .= $model->getCreateSQL() . ";\n";
+
+echo '$sql: ' . $sql;
+*/
