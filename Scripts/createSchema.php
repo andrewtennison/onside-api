4ffc8444fae5a7f3690b164fd62bdb0cf0d72c28
Isaac Scott <isaac.scott@anobii.com>
2011-11-02 21:41:39 +0000

    Scripts to drop/create schema

---
+++ Scripts/createSchema.php
@@ -0,0 +1,20 @@
+<?php
+include_once __DIR__ . '/../bootstrap.php';
+
+$mapper = new \Onside\Mapper\Article($db);
+$model = \Onside\Model\Article::getModelFromArray(array());
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\Channel($db);
+$model = \Onside\Model\Channel::getModelFromArray(array());
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\Discussion($db);
+$model = \Onside\Model\Discussion::getModelFromArray(array());
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\Event($db);
+$model = \Onside\Model\Event::getModelFromArray(array());
+$sql .= $model->getCreateSQL() . ";\n";
+$mapper = new \Onside\Mapper\User($db);
+$model = \Onside\Model\User::getModelFromArray(array());
+$sql .= $model->getCreateSQL() . ";\n";
+
+$db->exec($sql);
