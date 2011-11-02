de9a2f6f85d464220bff0b0fc2b98ae47dd802c7
Isaac Scott <isaac.scott@anobii.com>
2011-11-02 14:55:07 +0000

    Updated data structures

--- Onside/Model/Channel.php
+++ Onside/Model/Channel.php
@@ -8,10 +8,23 @@ class Channel extends Model
     protected $_definitions = array(
         'id' => 'INT(11) NOT NULL AUTO_INCREMENT',
         'name' => 'VARCHAR(100)',
+        'description' => 'TEXT',
+        'sport' => 'VARCHAR(50)',
+        'type' => 'VARCHAR(50)',
+        'level' => 'INT(1) NOT NULL default 0',
+        'geolat' => 'DECIMAL(10,6) default NULL',
+        'geolng' => 'DECIMAL(10,6) default NULL',
+
 //        'added' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP',
     );
     
     public $id;
     public $name;
+    public $description;
+    public $sport;
+    public $type;
+    public $level;
+    public $geolat;
+    public $geolng;
 //    public $added;
 }
--- Onside/Model/Event.php
+++ Onside/Model/Event.php
@@ -8,10 +8,18 @@ class Event extends Model
     protected $_definitions = array(
         'id' => 'INT(11) NOT NULL AUTO_INCREMENT',
         'name' => 'VARCHAR(100)',
+        'sport' => 'VARCHAR(50)',
+        'type' => 'VARCHAR(50)',
+        'geolat' => 'DECIMAL(10,6) default NULL',
+        'geolng' => 'DECIMAL(10,6) default NULL',
 //        'added' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP',
     );
     
     public $id;
     public $name;
+    public $sport;
+    public $type;
+    public $geolat;
+    public $geolng;
 //    public $added;
 }
