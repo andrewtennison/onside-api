88a92d697d52f6868dd90d51d5b6106ea8cbb69f
Isaac Scott <isaac.scott@anobii.com>
2011-11-09 15:37:15 +0000

    Updated test bootstrap to recreate unit test db structure plus necessary records each run

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -15,31 +15,37 @@ set_include_path(APPLICATION_BASE . '/Tests' . PATH_SEPARATOR . get_include_path
 set_include_path(APPLICATION_BASE . '/Tests/Api' . PATH_SEPARATOR . get_include_path());
 
 // Reset database
-/**
 use \Onside\Db;
 $db = new Db('mysql:host=127.0.0.1;user=onside;pass=On2011Side;dbname=onside_unittest', 'onside', 'On2011Side');
 $db->setAttribute(Db::ATTR_ERRMODE, Db::ERRMODE_EXCEPTION);
 
-$mapper = new \Onside\Mapper\Article($db);
 $model = \Onside\Model\Article::getModelFromArray(array());
 $sql = $model->getDropSQL() . ";\n";
 $sql .= $model->getCreateSQL() . ";\n";
-$mapper = new \Onside\Mapper\Channel($db);
 $model = \Onside\Model\Channel::getModelFromArray(array());
 $sql .= $model->getDropSQL() . ";\n";
 $sql .= $model->getCreateSQL() . ";\n";
-$mapper = new \Onside\Mapper\Discussion($db);
 $model = \Onside\Model\Discussion::getModelFromArray(array());
 $sql .= $model->getDropSQL() . ";\n";
 $sql .= $model->getCreateSQL() . ";\n";
-$mapper = new \Onside\Mapper\Event($db);
 $model = \Onside\Model\Event::getModelFromArray(array());
 $sql .= $model->getDropSQL() . ";\n";
 $sql .= $model->getCreateSQL() . ";\n";
-$mapper = new \Onside\Mapper\User($db);
 $model = \Onside\Model\User::getModelFromArray(array());
 $sql .= $model->getDropSQL() . ";\n";
 $sql .= $model->getCreateSQL() . ";\n";
+$model = \Onside\Model\Follower::getModelFromArray(array());
+$sql .= $model->getDropSQL() . ";\n";
+$sql .= $model->getCreateSQL() . ";\n";
 
+$db->exec($sql);
+
+$sql = <<<SQL
+INSERT INTO `channel` (`name`, `description`, `sport`, `type`, `level`) VALUES
+    ('Manchester United', 'Everything about manchester united', 'football', 'club', 1) ;
+SQL;
+$db->exec($sql);
+
+/**
 echo '$sql: ' . $sql;
 */
