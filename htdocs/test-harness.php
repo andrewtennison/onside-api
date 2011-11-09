f8f7fdc5fe5c7a16c73c46448398d32b3ad16939
Isaac Scott <isaac.scott@anobii.com>
2011-11-09 15:35:46 +0000

    Added test harness for testing API request / responses

---
+++ htdocs/test-harness.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <title>API - Test Harness</title>
+        
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+        <script>
+            var json;
+            $(document).ready(function(){
+                $('form').submit(function(){
+                    $('p.request').text($('input[name=url]').val());
+                    $.ajaxSetup({
+                        url: $('input[name=url]').val(),
+                        dataType: 'json',
+                        type: $('input:radio[name=method]:checked').val()
+                    });
+                    $.ajax({
+                        dataType: 'json',
+                        data: $('textarea[name=payload]').val(),
+                        dataFilter: function(data, type){
+                            json = data;
+                            $('p.response').text(data);
+                            return data;
+                        },
+                        success: function(data){
+                            //json = data;
+                            //$('textarea[name=response]').val(data.toString());
+                        }
+                    });
+                    return false;
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <h2>API - Test Harness</h2>
+        <form>
+            <label for="method">Method: </label>
+            <input type="radio" name="method" value="GET" checked="checked" /> - get
+            <input type="radio" name="method" value="POST" /> - post
+            <input type="radio" name="method" value="DELETE" /> - delete
+            <input type="radio" name="method" value="PUT" /> - put
+            
+            <button>Send Request</button>
+            
+            <br />
+            
+            <label for="service">API Call: </label>
+            <select name="service">
+                <option value="article">Article</option>
+                <option value="channel">Channel</option>
+                <option value="discussion">Discussion</option>
+                <option value="event">Event</option>
+                <option value="user">User</option>
+            </select>
+            
+            <label for="url">Url</label>
+            <input type="text" name="url" value="" />
+            
+            <br />
+            <!-- fluid variables //-->
+            
+        </form>
+        
+        
+        <!-- results //-->
+        <p class="request"></p>
+        <br />
+        <textarea name="payload" rows="10" cols="65"></textarea>
+        <br />
+        <p class="response"></p>
+        
+    </body>
+</html>
