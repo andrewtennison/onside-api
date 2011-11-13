6038250b4ff04dd97952e45d051b25fe370e6af4
Isaac Scott <isaac.scott@anobii.com>
2011-11-13 13:50:46 +0000

    Stack trace added to db structure

--- Onside/Model/Logging.php
+++ Onside/Model/Logging.php
@@ -9,11 +9,13 @@ class Logging extends Model
         'id' => 'INT(11) NOT NULL AUTO_INCREMENT',
         'code' => 'INT(4) NOT NULL',
 	'message' => 'TEXT NOT NULL',
+	'stacktrace' => 'TEXT',
         'added' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP',
     );
     
     public $id;
     public $code;
     public $message;
+    public $stacktrace;
     public $added;
 }
