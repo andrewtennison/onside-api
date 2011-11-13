0c1fd0671a2136575a7e7ed371f6b29d097255af
Isaac Scott <isaac.scott@anobii.com>
2011-11-13 13:40:39 +0000

    Refactored logging db structure

---
+++ Onside/Model/Logging.php
@@ -0,0 +1,19 @@
+<?php
+namespace Onside\Model;
+use \Onside\Model;
+
+class Logging extends Model
+{
+    protected $_table = 'logging';
+    protected $_definitions = array(
+        'id' => 'INT(11) NOT NULL AUTO_INCREMENT',
+        'code' => 'INT(4) NOT NULL',
+	'message' => 'TEXT NOT NULL',
+        'added' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP',
+    );
+    
+    public $id;
+    public $code;
+    public $message;
+    public $added;
+}
