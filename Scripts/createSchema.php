c6260c833a3b3e5cbda11a80b12d8c7fbfb3339f
Isaac Scott <isaac.scott@anobii.com>
2011-11-15 13:24:34 +0000

    Typo fix

--- Scripts/createSchema.php
+++ Scripts/createSchema.php
@@ -2,7 +2,7 @@
 include_once __DIR__ . '/../bootstrap.php';
 
 $model = \Onside\Model\Article::getModelFromArray(array());
-$sql .= $model->getCreateSQL() . ";\n";
+$sql = $model->getCreateSQL() . ";\n";
 $model = \Onside\Model\Channel::getModelFromArray(array());
 $sql .= $model->getCreateSQL() . ";\n";
 $model = \Onside\Model\Discussion::getModelFromArray(array());
