6eef5ba1c27a5d0a75c650cfa3caf7f4da512af2
Isaac Scott <isaac.scott@anobii.com>
2011-11-16 22:15:17 +0000

    Updated to accept tables as parameters, if set only tables passed will be processed

--- Scripts/dropSchema.php
+++ Scripts/dropSchema.php
@@ -1,17 +1,31 @@
 <?php
 include_once __DIR__ . '/../bootstrap.php';
 
-$model = \Onside\Model\Article::getModelFromArray(array());
-$sql = $model->getDropSQL() . ";\n";
-$model = \Onside\Model\Channel::getModelFromArray(array());
-$sql .= $model->getDropSQL() . ";\n";
-$model = \Onside\Model\Comment::getModelFromArray(array());
-$sql .= $model->getDropSQL() . ";\n";
-$model = \Onside\Model\Event::getModelFromArray(array());
-$sql .= $model->getDropSQL() . ";\n";
-$model = \Onside\Model\User::getModelFromArray(array());
-$sql .= $model->getDropSQL() . ";\n";
-$model = \Onside\Model\Follower::getModelFromArray(array());
-$sql .= $model->getDropSQL() . ";\n";
+$all = count($argv) === 1;
+
+if ($all || in_array('article', $argv)) {
+    $model = \Onside\Model\Article::getModelFromArray(array());
+    $sql = $model->getDropSQL() . ";\n";
+}
+if ($all || in_array('channel', $argv)) {
+    $model = \Onside\Model\Channel::getModelFromArray(array());
+    $sql .= $model->getDropSQL() . ";\n";
+}
+if ($all || in_array('comment', $argv)) {
+    $model = \Onside\Model\Comment::getModelFromArray(array());
+    $sql .= $model->getDropSQL() . ";\n";
+}
+if ($all || in_array('event', $argv)) {
+    $model = \Onside\Model\Event::getModelFromArray(array());
+    $sql .= $model->getDropSQL() . ";\n";
+}
+if ($all || in_array('user', $argv)) {
+    $model = \Onside\Model\User::getModelFromArray(array());
+    $sql .= $model->getDropSQL() . ";\n";
+}
+if ($all || in_array('follower', $argv)) {
+    $model = \Onside\Model\Follower::getModelFromArray(array());
+    $sql .= $model->getDropSQL() . ";\n";
+}
 
 $db->exec($sql);
