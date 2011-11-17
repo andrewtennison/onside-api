ce31e3998bc2e0464352017d07a9b17aa5368f1e
Isaac Scott <isaac.scott@anobii.com>
2011-11-17 18:44:22 +0000

    Change to auth logic for debug purposes

--- Api/BaseApi.php
+++ Api/BaseApi.php
@@ -83,11 +83,12 @@ class BaseApi
 	}
 	// TODO: client authentication throw 401
 	if ('OPTIONS' !== $this->request->getMethod()) {
-	$headers = apache_request_headers();
-	if (!array_key_exists('OnsideAuth', $headers) || $headers['OnsideAuth'] !== '01a2e0d73218f42d1495c3670b79f1bd44d7afa316340679bcd365468b736482') {
-	    $error = $this->errors->getError(101, array());
-	    throw new Exception(array($error->getResponse()), 401);
-	}
+	    $headers = apache_request_headers();
+	    if (!array_key_exists('OnsideAuth', $headers) || $headers['OnsideAuth'] !== '01a2e0d73218f42d1495c3670b79f1bd44d7afa316340679bcd365468b736482') {
+die(print_r($headers, true));
+//		$error = $this->errors->getError(101, array());
+//		throw new Exception(array($error->getResponse()), 401);
+	    }
 	}
 	
 	// basic auth
