1e32b371d7aeb47ab5c9a2617ab39826771d5056
Lucian Holland <lucian@symposion.co.uk>
2012-02-27 19:24:23 +0000

    #12 allow sorting by stime for events

--- Onside/Api/EventController.php
+++ Onside/Api/EventController.php
@@ -5,36 +5,36 @@ use \Onside\Mapper\Event;
 
 class EventController extends BaseController
 {
-    protected $filters = array('sport', 'type', 'channel');
+    protected $filters = array('sport', 'type', 'channel', 'stime');
     private $_mapper;
-    
+
     public function __construct()
     {
         global $db;
         $this->_mapper = new Event($db);
     }
-    
+
         public function actionChannel($id, $data)
     {
 	$this->results[] = $this->_mapper->addChannel($data['channel'], $data['event']);
     }
-    
+
     public function actionNochannel($id, $data)
     {
 	$this->results[] = $this->_mapper->removeChannel($data['channel'], $data['event']);
     }
-    
+
     public function actionDelete($id)
     {
 	$this->results[] = $this->_mapper->deleteItem($id);
     }
-    
+
     public function actionGet($data = array())
     {
 	$where = $this->getAcceptedFilters($data);
-	$this->results[] = $this->_mapper->selectItem($where, array(), $this->limit);
+	$this->results[] = $this->_mapper->selectItem($where, $this->getSort($data), $this->limit);
     }
-    
+
     public function actionItem($id)
     {
 	$this->results[] = $this->_mapper->getItem($id);
@@ -44,7 +44,7 @@ class EventController extends BaseController
     {
 	$this->results[] = $this->_mapper->updateItem($id, $data);
     }
-    
+
     public function actionPut($data)
     {
 	$this->results[] = $this->_mapper->addItem($data);
