5bc9f037aa1e9d491b2efed6c7278e0ab8aefe79
Lucian Holland <lucian@symposion.co.uk>
2012-02-29 11:09:10 +0000

    Change email address to official one.

--- Onside/Email.php
+++ Onside/Email.php
@@ -29,7 +29,7 @@ class Email
 
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/plain; charset=iso-8859-1' . "\r\n";
-	$headers .= 'From: devops@onside.me' . "\r\n";
+	$headers .= 'From: please.reply@onside.me' . "\r\n";
 	if (!empty($this->template->cc))
 		$headers .= 'Cc: ' . $this->template->cc . "\r\n";
 	if (!empty($this->template->bcc))
